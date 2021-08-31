41de30c110a3469e71ed2be077991ddab93199df
+ <+>
2021-08-31 10:27:29 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRoute, BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+import App from "./App"
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root"));
